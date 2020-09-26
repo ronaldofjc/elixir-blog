d329e0b6d268bf0abea88488a6c26c0a83be4be2
Ronaldo <rfeitosa@ciandt.com>
2020-09-25 21:52:19 -0300

    First app with elixir and phoenix

---
+++ lib/blog/my_blog/post.ex
@@ -0,0 +1,20 @@
+defmodule Blog.MyBlog.Post do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "posts" do
+    field :body, :string
+    field :title, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(post, attrs) do
+    post
+    |> cast(attrs, [:title, :body])
+    |> validate_length(:title, min: 6)
+    |> validate_length(:title, max: 30)
+    |> validate_required([:title, :body])
+  end
+end
